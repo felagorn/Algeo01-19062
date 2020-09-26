0bcc22a2ae8346739efbb2ddc7dcacec6cc3bd60
felagorn <13519062@std.stei.itb.ac.id>
2020-09-26 22:40:34 +0700

    Update 26/9/2020 #3
    1. NEW class Determinant!
    2. Add method Convert_Minor to class Matrix
    3. Add this. argument to most method calls in class Matrix

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import flib.Matrix;
+import flib.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Matrix matrix = new Matrix(4, 5);
         matrix.DF = new double[][] { {8,-9,1,-8,80},
-                                     {-3,-1,5,4,7},
+                                     {0,-1,5,4,7},
                                      {-2,-1,-3,8,-30},
                                      {-2,-8,-1,2,18} };
         System.out.println("Matriks awal:");
@@ -22,6 +22,25 @@ public class Main {
         matrix.Convert_ReducedRowEchelon();
         System.out.println("Matriks setelah diubah ke dalam bentuk eselon baris tereduksi:");
         matrix.PrintMatrix();
+        System.out.printf("\n\n");
+        Matrix squarematrix = new Matrix(5, 5);
+        squarematrix.DF = new double[][] {  {1, 3, 1, 5, 3},
+                                            {-2, -7, 0, -4, 2},
+                                            {0, 0, 1, 0, 1},
+                                            {0, 0, 2, 1, 1},
+                                            {0, 0, 0, 1, 1}     };
+        System.out.println("Matrix determinan awal:");
+        squarematrix.PrintMatrix();
+        System.out.printf("\n\n");
+        Determinant obeDeterminant = new Determinant();
+        obeDeterminant.OBE(squarematrix);
+        System.out.print("Determinan OBE: ");
+        obeDeterminant.PrintDeterminant();
+        System.out.println();
+        Determinant cofactorDeterminant = new Determinant();
+        cofactorDeterminant.Kofaktor(squarematrix);
+        System.out.print("Determinan Kofaktor: ");
+        cofactorDeterminant.PrintDeterminant();
         System.out.println();
     }
 }
---
+++ src/flib/Determinant.java
@@ -0,0 +1,68 @@
+package flib;
+
+public class Determinant {
+    double DET;
+    public Determinant() {
+        this.DET = 0;
+    }
+    public void OBE(Matrix matrix) {
+        if (matrix.GetROWCOUNT() == matrix.GetCOLCOUNT()) {
+            Matrix upperTriangle = new Matrix(matrix.GetROWCOUNT(), matrix.GetCOLCOUNT());
+            upperTriangle.PasteDFFrom(matrix);
+            int iInitial = upperTriangle.GetFirstRowID();
+            int jInitial = upperTriangle.GetFirstColID();
+            int iFinal = upperTriangle.GetLastRowID();
+            int jFinal = upperTriangle.GetLastColID();
+            for (int j = jInitial, iCurrent = iInitial; j <= jFinal; j += 1, iCurrent += 1) {
+                for (int i = iCurrent + 1; i <= iFinal; i += 1) {
+                    double k = upperTriangle.GetElement(i, j) / upperTriangle.GetElement(iCurrent, j);
+                    upperTriangle.OBE_SumRow(i, iCurrent, -k);
+                }
+            }
+            this.DET = 1;
+            for (int i = iInitial, j = jInitial; ((i <= iFinal) && (j <= jFinal)); i += 1, j += 1) {
+                this.DET *= upperTriangle.GetElement(i, j);
+            }
+        }
+        else {
+            System.out.println("ERROR: Undefined determinant. Matrix must be n x n in dimension.");
+        }
+    }
+
+    private double Kofaktor_Calculator(Matrix matrix) {
+        double recurse;
+        if ((matrix.GetROWCOUNT() == 2) && (matrix.GetCOLCOUNT() == 2)) {
+            int i1 = matrix.GetFirstRowID();
+            int i2 = matrix.GetLastRowID();
+            int j1 = matrix.GetFirstColID();
+            int j2 = matrix.GetLastColID();
+            recurse = (matrix.GetElement(i1, j1) * matrix.GetElement(i2, j2)) - (matrix.GetElement(i2, j1) * matrix.GetElement(i1, j2));
+        }
+        else {
+            int jFinal = matrix.GetLastColID();
+            int i = matrix.GetFirstRowID();
+            double d = 0;
+            for (int j = matrix.GetFirstColID(); j <= jFinal; j += 1) {
+                Matrix minor = new Matrix((matrix.GetROWCOUNT()), (matrix.GetCOLCOUNT()));
+                minor.PasteDFFrom(matrix);
+                minor.Convert_Minor(i, j);
+                d += (Math.pow(-1, ((i + 1) + (j + 1))) * matrix.GetElement(i, j) * Kofaktor_Calculator(minor));
+            }
+            recurse = d;
+        }
+        return recurse;
+    }
+
+    public void Kofaktor(Matrix matrix) {
+        if (matrix.GetROWCOUNT() == matrix.GetCOLCOUNT()) {
+            this.DET = Kofaktor_Calculator(matrix);
+        }
+        else {
+            System.out.println("ERROR: Undefined determinant. Matrix must be n x n in dimension.");
+        }
+    }
+
+    public void PrintDeterminant() {
+        System.out.printf("%f", this.DET);
+    }
+}
