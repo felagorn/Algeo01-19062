8fa5084a48f4276d177978855c88479f6394a443
jonathanrichard13 <64012903+jonathanrichard13@users.noreply.github.com>
2020-10-02 00:04:38 +0700

    chaos

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     }
     
     private static void MainMenu() {
-        //clrscr();
+  
         System.out.println("MENU");
         System.out.println("1. Sistem Persamaan Linier");
         System.out.println("2. Determinan");
@@ -27,9 +27,11 @@ public class Main {
                 System.out.println("ERROR: Input tidak valid");
             }
         } while (!(q.equals("1") || q.equals("2") || q.equals("3") || q.equals("4") || q.equals("5") || q.equals("6")));
-        input.close();
+        
         if (q.equals("1")) {
-
+   
+            Menu1();
+            input.close();
         } else if (q.equals("2")) {
 
         } else if (q.equals("3")) {
@@ -43,44 +45,99 @@ public class Main {
         }
     }
 
+    private static void Menu1(){
+ 
+        System.out.println("MENU 1");
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode eliminasi Gauss-Jordann");
+        System.out.println("3. Metode matriks balikan");
+        System.out.println("4. Kaidah Cramer");
+        System.out.println("----------------------------------------------------------------");
+        Scanner input = new Scanner(System.in);
+        String q;
+        do {
+            System.out.print("Pilihan anda: ");
+            q = input.nextLine();
+
+            if (!(q.equals("1") || q.equals("2") || q.equals("3") || q.equals("4"))) {
+                System.out.println("ERROR: Input tidak valid");
+            }
+        } while (!(q.equals("1") || q.equals("2") || q.equals("3") || q.equals("4")));
+        
+        if (q.equals("1")) {
+            MenuGauss();
+            input.close();
+        } else if (q.equals("2")) {
+
+        } else if (q.equals("3")) {
+
+        } else{
+
+        }
+
+    }
+
+    public static void MenuGauss(){
+        System.out.println("Sistem Persamaan Linear - Metode eliminasi Gauss");
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Pilih metode input:");
+        System.out.println("1. Input dari keyboard");
+        System.out.println("2. Input dari file txt");
+        System.out.println("3. Back");
+
+        Scanner input = new Scanner(System.in);
+        String q;
+        do {
+            System.out.print("Pilihan anda: ");
+            q = input.nextLine();
+            if (!(q.equals("1") || q.equals("2")) || q.equals("3")) {
+                System.out.println("ERROR: Input tidak valid");
+            }
+        } while (!(q.equals("1") || q.equals("2") || q.equals("3")));
+        Matrix M=new Matrix(0,0);
+        if (q.equals("1")) {    
+            M.bacaMatriks();
+            
+        } else if (q.equals("2")) {
+            M = Matrix.Create_FromTxt();
+
+        } else{
+            Menu1();
+        }
+        if (M.ROWCOUNT > M.COLCOUNT){
+            if (!M.checkMatrix()){
+                System.out.println("SPL tak memiliki solusi");
+                System.out.println("----------------------------------------------------------------");
+                System.out.println("Tekan Y bila ingin kembali ke menu utama");
+                System.out.println("Tekan sembarang untuk keluar");
+                Scanner input1 = new Scanner(System.in);
+                System.out.print("Pilihan anda: ");
+                q = input1.nextLine();
+                if (q.equals("Y")){
+                    input1.close();
+                    MainMenu();
+                }else{
+                    System.exit(0);
+                }
+            }
+        }
+        M.Convert_RowEchelon();
+        M.ParametricOnGauss();
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Tekan Y bila ingin kembali ke menu utama");
+        System.out.println("Tekan sembarang untuk keluar");
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Pilihan anda: ");
+        q = input1.nextLine();
+        if (q.equals("Y")){
+            input1.close();
+            MainMenu();
+        }else{
+            System.exit(0);
+        }
+
+    }
     public static void main(String[] args) {
         MainMenu();
-        //readFromTxt test
-        //System.out.println("Coba readTxt:\n");
-        //Matrix matrix = Matrix.Create_FromTxt();
-        //"C:\\Users\\ASUS ROG GL503G\\Algeo01-19062\\test\\isi.txt"
-        //matrix.PrintMatrix();
-        //System.out.println("\n");
-        
-        //Interpolation test
-        System.out.println("Coba interpolasi:\n");
-        Matrix manual = new Matrix(3, 2);
-        manual.DF = new double[][] {    {8.0, 2.0794}, 
-                                        {9.0, 2.1972}, 
-                                        {9.5, 2.2513}};
-        manual.PrintMatrix();
-        System.out.println("\n");
-        
-        Interpolation i = new Interpolation(manual);
-        i.INTERPOLATIONMATRIX.PrintMatrix();;
-        System.out.println("\n");
-        double y = i.Interpolate(9.2);
-        System.out.println("\n");
-        System.out.println(y);
-        System.out.println("\n");
-
-        //Matriks Hilbert
-        System.out.println("Coba hilbert:\n");
-        Matrix M1 = Matrix.makeHilbertAugmented(6);
-        M1.PrintMatrix();;
-        System.out.println("\n");
-
-        //Parametric test
-        Matrix M2 = new Matrix(4,5);
-        M2.DF = new double[][] {    {1,2,3,4,5},
-                                    {0,1,0,1,2},
-                                    {0,0,0,1,1},
-                                    {0,0,0,0,0}};
-        M2.ParametricOnGauss();
     }
 }
